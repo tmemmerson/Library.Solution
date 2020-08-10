1aa2913a542fa75a54a551b5a0594941f3a8d7cc
Tristan Emmerson <tristan@stickerslug.com>
2020-08-10 15:36:03 -0700

    update librarycontext virtual

--- Library/Models/LibraryContext.cs
+++ Library/Models/LibraryContext.cs
@@ -5,9 +5,9 @@ namespace Library.Models
 {
   public class LibraryContext : IdentityDbContext<ApplicationUser>
   {
-    public DbSet<Author> Authors { get; set; }
-    public DbSet<Book> Books { get; set; }
-    public DbSet<AuthorBook> AuthorBook {get; set; }
+    public virtual DbSet<Author> Authors { get; set; }
+    public virtual DbSet<Book> Books { get; set; }
+    public virtual DbSet<AuthorBook> AuthorBook {get; set; }
 
     public LibraryContext(DbContextOptions options) : base(options) {}
   }
