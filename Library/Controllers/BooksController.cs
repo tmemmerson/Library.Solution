2a55e9b2e8f54f19b5bcf70a15735db6c13e2dab
Tristan Emmerson <tristan@stickerslug.com>
2020-08-10 10:59:42 -0700

    equalize

---
+++ Library/Controllers/BooksController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using Library.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Controllers
+{
+  
+}
