4d3dffd9b5c8da91c260cd810b532d3e5f304d51
Tristan Emmerson <tristan@stickerslug.com>
2020-08-10 11:26:38 -0700

    create books controller

--- Library/Controllers/BooksController.cs
+++ Library/Controllers/BooksController.cs
@@ -6,5 +6,109 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Library.Controllers
 {
-  
+  [Authorize]
+  public class BooksController : Controllers
+  {
+    private readonly LibraryContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public BooksController(UserManager<ApplicationUser> userManager, LibraryContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Books.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.AuthorId = newSelectList(_db.Authors, "AuthorId", "Name");
+      return View();
+    }
+
+    [HttPost]
+    public ActionResult Create(Book book, int AuthorId)
+    {
+      _db.Books.Add(book);
+      if (AuthorId != 0)
+      {
+        _db.AuthorBook.Add(new AuthorBook () {AuthorId = AuthorId, BookId = book.BookId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisBook = _db.Books  
+        .Include(book => book.Authors)
+        .ThenInclude(join => join.Author)
+        .FirstOrDefault(Book => book.BookId == id);
+      return View(thisBook);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisBook = _db.Books.FirstOrDefault(BooksController => BooksController.BookId == id);
+      ViewBag.AuthorId = new SelectList(_db.Authors, "AuthorId", "Name");
+      return ViewModels(thisBook);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Book book, int AuthorId)
+    {
+      if (AuthorId != 0)
+      {
+        __db.AuthorBook.Add(new AuthorBook() {AuthorId = AuthorId, BookId = book.BookId});
+      }
+      _db.Entry(book).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddAuthor(int id)
+    {
+      var thisBook = _db.Books.FirstOrDefault(books => books.BookId == id);
+      ViewBag.AuthorId = new SelectList(_db.Authors, "AuthorId", "Name");
+      return View(thisBook);
+    }
+
+    [HttpPost]
+    public ActionResult AddAuthor(Book book, int AuthorId)
+    {
+      if (AuthorId != 0)
+      {
+        _db.AuthorBook.Add(new AuthorBook() {AuthorId = AuthorId, BookId = book.BookId});
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisBook = _db.Books.FirstOrDefault(books => books.BookId == id);
+      return ViewModels(thisBook);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisBook = _db.Books.FirstOrDefault(books => books.BookId == id);
+      _db.Books.Remove(thisBook);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteAuthor(int joinId)
+    {
+      var joinEntry = _db.AuthorBook.FirstOrDefault(entry => entry.AuthorBookId == joinId);
+      _db.AuthorBook.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
 }
