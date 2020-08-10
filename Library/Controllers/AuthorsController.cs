a7f484f4fff3da6c72a740dc8d38e614653af850
Tristan Emmerson <tristan@stickerslug.com>
2020-08-10 15:35:18 -0700

    add search filtering functionality.

--- Library/Controllers/AuthorsController.cs
+++ Library/Controllers/AuthorsController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Library.Controllers
 {
