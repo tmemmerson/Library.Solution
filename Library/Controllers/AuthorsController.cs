a30d5727d157fe6eb2a8d6b65ba11861920e209d
Tristan Emmerson <tristan@stickerslug.com>
2020-08-10 15:04:30 -0700

    update authors controller for regex search

--- Library/Controllers/AuthorsController.cs
+++ Library/Controllers/AuthorsController.cs
@@ -3,6 +3,7 @@ using Library.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
 
 namespace Library.Controllers
 {
@@ -15,9 +16,15 @@ namespace Library.Controllers
       _db = db;
     }
 
-    public ActionResult Index()
+    public ActionResult Index(string Name)
     {
-      List<Author> model = _db.Authors.ToList();
+      IQueryable<Author> authorQuery = _db.Authors;
+      if (!string.IsNullOrEmpty(Name))
+      {
+        Regex search = new Regex(Name, RegexOptions.IgnoreCase);
+        authorQuery = authorQuery.Where(authors => search.IsMatch(authors.Name));
+      }
+      IEnumerable<Author> model = authorQuery.ToList().OrderBy(author => author.Name);
       return View(model);
     }
 
