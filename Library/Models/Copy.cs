b3d68e369c4a41c47b3ac16007a2e9ddd023b0e9
Tristan Emmerson <tristan@stickerslug.com>
2020-08-11 10:32:25 -0700

    create copy model

---
+++ Library/Models/Copy.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace Library.Models
+{
+  public class Copy
+  {
+    public int CopyId { get; set; }
+    public int BookId { get; set; }
+    public Book Book { get; set; }
+  }
+}
