97427ba42d74a27cc9c54db35f43989e40f2b4d2
Tristan Emmerson <tristan@stickerslug.com>
2020-08-12 10:09:29 -0700

    abandon branch in lieu of restart on master branch

---
+++ Library/Migrations/20200812165129_Seventh.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Library.Migrations
+{
+    public partial class Seventh : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Copies",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PatronId",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Checkouts",
+                columns: table => new
+                {
+                    CheckoutId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    PatronId = table.Column<int>(nullable: false),
+                    CopyId = table.Column<int>(nullable: false),
+                    PatronId1 = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Checkouts", x => x.CheckoutId);
+                    table.ForeignKey(
+                        name: "FK_Checkouts_Copies_CopyId",
+                        column: x => x.CopyId,
+                        principalTable: "Copies",
+                        principalColumn: "CopyId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Checkouts_AspNetUsers_PatronId1",
+                        column: x => x.PatronId1,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Copies_UserId",
+                table: "Copies",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Checkouts_CopyId",
+                table: "Checkouts",
+                column: "CopyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Checkouts_PatronId1",
+                table: "Checkouts",
+                column: "PatronId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Copies_AspNetUsers_UserId",
+                table: "Copies",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Copies_AspNetUsers_UserId",
+                table: "Copies");
+
+            migrationBuilder.DropTable(
+                name: "Checkouts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Copies_UserId",
+                table: "Copies");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Copies");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PatronId",
+                table: "AspNetUsers");
+        }
+    }
+}
